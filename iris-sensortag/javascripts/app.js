df5ca59314ffaff157b3868936395cf1943df7d9
Harrison Chen <harrisonxchen@gmail.com>
2014-11-09 09:41:07 -0800

    dude

--- iris-sensortag/javascripts/app.js
+++ iris-sensortag/javascripts/app.js
@@ -5,6 +5,13 @@ app.controller('MainCtrl', ['$scope', '$window', function($scope, $window){
 	var finishScroll = false;
 
 	var titles  = document.getElementsByClassName("title");
+	var height = $window.innerHeight;
+	var p9 = height + height / 2;
+	var p2 = p9 + height;
+	var p3 = p2 + height;
+	var p4 = p3 + height;
+	var p10 = p4 + height;
+
 	var parallax_2 = titles[1];
 	var parallax_10 = titles[2];
 	$scope.class_2 = false;
@@ -15,12 +22,12 @@ app.controller('MainCtrl', ['$scope', '$window', function($scope, $window){
   var parallax_10_pos = parallax_2_pos + titles[2].offsetTop + titles[2].clientHeight;
 
 	$scope.checkPositions = function(){
-		if(($scope.scrollPos + $window.innerHeight) >= (parallax_2_pos - 100)){
+		if(($scope.scrollPos + $window.innerHeight) >= p2){
 			$scope.class_2 = true;
 			$scope.class_android = true;
 			$scope.$apply();
 		}
-		if(($scope.scrollPos + $window.innerHeight) >= (parallax_10_pos + 100)){
+		if(($scope.scrollPos + $window.innerHeight) >= p10){
 			$scope.class_10 = true;
 			$scope.$apply();
 			finishScroll = true;
